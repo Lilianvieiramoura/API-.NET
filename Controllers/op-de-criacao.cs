3f6fc5c59aa5e7843f7cef1586b4cfa04fed67b4
Lílian Vieira <lilianv528@gmail.com>
2024-01-06 18:01:20 -0300

    initial commit

---
+++ Controllers/op-de-criacao.cs
@@ -0,0 +1,33 @@
+namespace TestApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using TestApi.Client;
+
+[ApiController]
+[Route("clients")]
+public class ClientController : ControllerBase
+{
+  private static List<Client> _clients = new();
+  private static int _nextId = 1;
+
+  [HttpPost]
+  public ActionResult Create(ClientRequest request)
+  {
+    var client = request.CreateClient(_nextId++);
+    _clients.Add(client);
+
+    return StatusCode(201, client);
+  }
+
+  [HttpPut("{id}")]
+  public ActionResult Update(int id, ClientRequest request)
+  {
+    var client = _clients.FirstOrDefault(c => c.Id == id);
+
+    if (client == null)
+      return NotFound("Client not found");
+
+    var clientUpdated = request.UpdateClient(client);
+
+    return Ok(clientUpdated);
+  }
+}
